f4a61c49e5ed1789f9f95a4c35acfc479de2f85f
Cleiton Arcanjo <kleytonarcanjo2002@gmail.com>
2026-06-24 11:49:32 -0300

    fix: corrige o valor da margem no layout

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <>
       <NavBar />
 
-      <div className='grid grid-cols-1 md:grid-cols-[1.6fr_1fr]  items-start gap-4 md:gap-8 mx-24 mt-4'>
+      <div className='grid grid-cols-1 md:grid-cols-[1.6fr_1fr]  items-start gap-4 md:gap-8 md:mx-24 mt-4'>
         <div className='row-span-2 h-full'>
           <Verificador />
         </div>
